896f04fe7a88ce0c8df6ea8745091aad9166e197
zE85S6U_work <sakieki@n3c.co.jp>
2020-06-08 14:48:25 +0900

    一端の完成

--- src/classes/Controllers/UserController.php
+++ src/classes/Controllers/UserController.php
@@ -12,26 +12,30 @@ use Slim\Http\Response;
 class UserController extends Controller
 {
     // 新規登録画面
-    public function index(Request $request, Response $response) {
+    public function index(Request $request, Response $response)
+    {
         return $this->renderer->render($response, '/user/signup.phtml');
     }
 
     // ユーザを登録
-    public function store(Request $request, Response $response) {
+    public function store(Request $request, Response $response)
+    {
         $loginid = e($request->getParsedBodyParam('login_id'));
-        $password = password_hash($request->getParsedBodyParam('password'), PASSWORD_DEFAULT );
+        $password = password_hash($request->getParsedBodyParam('password'), PASSWORD_DEFAULT);
 
         $sql = 'INSERT INTO m_user (login_id, password) VALUES (:login_id, :password)';
         $stmt = $this->db->prepare($sql);
 
         // プリペアードステートメントを安全に代入
-        $stmt->bindParam(':login_id', $loginid, PDO::PARAM_STR);
-        $stmt->bindParam(':password',$password , PDO::PARAM_STR);
-        $result = $stmt->execute();
-
-        if (!$result) {
-            throw new Exception
-            ('could not save the product');
+        try {
+            $stmt->bindParam(':login_id', $loginid, PDO::PARAM_STR);
+            $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+            $stmt->execute();
+        } catch (Exception $e) {
+            return $response
+                ->withStatus(500)
+                ->withHeader('Content-Type', 'text/html')
+                ->write('問題が発生しました:エラーコード[' . $e->getCode() . ']');
         }
 
         // 保存が正常に出来たらTOPページへリダイレクトする
@@ -39,12 +43,14 @@ class UserController extends Controller
     }
 
     // ログインページへ
-    public function show(Request $request, Response $response) {
+    public function show(Request $request, Response $response)
+    {
         return $this->renderer->render($response, '/user/login.phtml');
     }
 
     // ログイン
-    public function login(Request $request, Response $response) {
+    public function login(Request $request, Response $response)
+    {
         $loginid = e($request->getParsedBodyParam('login_id'));
         $password = $request->getParsedBodyParam('password');
 
@@ -56,17 +62,18 @@ class UserController extends Controller
 
         if ($user && password_verify($password, $user['password'])) {
             $_SESSION['user']['user_id'] = (int)$user['user_id'];
-            $_SESSION['user']['login_id']= $user['login_id'];
-            $_SESSION['user']['auth']= $user['auth'];
+            $_SESSION['user']['login_id'] = $user['login_id'];
+            $_SESSION['user']['auth'] = $user['auth'];
             // 正常に認証出来たらTOPページへリダイレクトする
             return $response->withRedirect("/");
         } else {
-        echo 'ログインできません　ログイン名かパスワードを確認してください。';
+            echo 'ログインできません　ログイン名かパスワードを確認してください。';
         }
     }
 
     // ログアウト
-    public function logout(Request $request, Response $response) {
+    public function logout(Request $request, Response $response)
+    {
         unset($_SESSION['user']);
         return $response->withRedirect("/");
     }
